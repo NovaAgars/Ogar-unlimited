b7be3507c0595ae6886200aa0c7b7d08ad89efee
Andrew54757 <andrews54757@gmail.com>
2016-06-01 11:12:33 -0400

    Update PacketHandler.js

--- src/core/PacketHandler.js
+++ src/core/PacketHandler.js
@@ -126,6 +126,7 @@ PacketHandler.prototype.handleMessage = function (message) {
           c.borderTop + this.socket.playerTracker.scrambleY,
           c.borderBottom + this.socket.playerTracker.scrambleY
         ));
+        this.socket.sendPacket(new Packet.DataPacket(this.gameServer));
       }
       break;
        case 90: // from cigar
